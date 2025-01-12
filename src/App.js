9946948e3077d2a6599a0f4531df08a32b842efc
puneet <puneet.r97@gmail.com>
2025-01-12 22:13:52 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with a bug fix
+          Learn React with a bug fix and commits
         </a>
       </header>
     </div>
