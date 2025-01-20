a4d44659d673de65b53e05e339375286724f7963
puneet <puneet.r97@gmail.com>
2025-01-20 18:22:16 +0530

    commit 20th jan

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with a bug fix
+          Learn React with a bug fix fix on 20th jan
         </a>
       </header>
     </div>
