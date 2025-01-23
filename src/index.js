3b36ddf7e893e2ef7de6a21bfdc8935de580a788
puneet <puneet.r97@gmail.com>
2025-01-23 21:20:45 +0530

    push from bug fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
 );
 
 //wrap with Auth context and logger and apply fix on 20th Jan
-
+// bug fix 23rd jan
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
